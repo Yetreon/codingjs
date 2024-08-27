f9b0ba83a5fc60df6d9b8c0f086677bd33afcaf5
Yetreon <70313461+Yetreon@users.noreply.github.com>
2024-08-27 17:08:47 +0200

    bugfix in solution for makeChocolate

--- src/data/logicSolutions.js
+++ src/data/logicSolutions.js
@@ -399,13 +399,14 @@ solutions.cigarParty = function (cigars, isWeekend) {
   
   solutions.makeChocolate = function (small, big, goal) {
       // not enough
-    if (small + big * 5 < goal) {
-      return -1;
-    }
   
     const bigsNeeded = Math.floor(goal / 5);
   
     const bigsUsed = Math.min(big, bigsNeeded);
+
+    if (small + bigsUsed * 5 < goal) {
+      return -1;
+    }
      // how many leftover
     const smallsNeeded = goal - bigsUsed * 5;
   
